89ba610dc38e35a2ddaff124ff700e130fb29389
dev-mint <studiominti@gmail.com>
2020-01-24 15:22:11 -0300

    First Commit

---
+++ appointment.rb
@@ -0,0 +1,73 @@
+#############################
+    #APPOINTMENT
+############################
+class Appointment
+    attr_accessor :location, :purpose, :hour, :min
+    def initialize(location, purpose, hour, min)
+        @location = location
+        @purpose = purpose
+        @hour = hour
+        @min = min
+    end
+end
+
+#############################
+    #MONTHLY APPOINTMENT
+############################
+class MonthlyAppointment < Appointment
+    attr_reader :day
+    def initialize(location, purpose, day, hour, min)
+        super(location, purpose, hour, min)
+        @day = day
+    end
+
+    def occurs_on?(day)
+        @day == day
+    end
+
+    def to_s
+        "Reunión mensual en #{location} sobre #{purpose} el día #{day} a la(s) #{hour}:#{min}"
+    end
+end
+
+#############################
+    #DAILY APPOINTMENT
+############################
+class DailyAppointment < Appointment
+    def occurs_on?(hour, min)
+        @hour == hour && @min == min
+    end
+    def to_s
+        "Reunión diaria en #{location} sobre #{purpose} a la(s) #{hour}:#{min}"
+    end
+end
+
+#############################
+    #ONE TIME APPOINTMENT
+############################
+class OneTimeAppointment < Appointment
+    attr_reader :day, :month, :year
+    def initialize(location, purpose, hour, min, day, month, year)
+        super(location, purpose, hour, min)
+        @day = day
+        @month = month
+        @year = year
+    end
+
+    def occurs_on?(day, month, year)
+        @day == day && @month == month && @year == year
+
+    end
+    
+    def to_s
+        "Reunión única en #{location} sobre #{purpose} el #{day}/#{month}/#{year} a la(s) #{hour}:#{min}"
+    end
+end
+
+#############################
+    #CALLING OBJECTS
+############################
+
+puts OneTimeAppointment.new('Desafío Latam', 'Trabajo', 14, 30, 4, 6, 2019)
+puts DailyAppointment.new('Desafío Latam', 'Educación', 8, 15)
+puts MonthlyAppointment.new('NASA', 'Aliens', 8, 15, 23)
